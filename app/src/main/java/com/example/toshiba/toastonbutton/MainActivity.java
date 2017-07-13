2df2251465512ec2f66c6467595536542a346920
Nabeel Yaseen <nabeel.yaseen10@gmail.com>
2017-07-13 06:15:45 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/toshiba/toastonbutton/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.toshiba.toastonbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MTAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
+
+//      this.findViewById(R.id.btn_submit);
+
+        Button btn_submit = (Button) findViewById(R.id.btn_submit);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button Pressed!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
